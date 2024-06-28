c56a758d3d9491de990985048815778f330bd49c
carlos4565mana <carloscal61@gmail.com>
2024-06-28 10:30:30 -0300

    create TopicStatus enum and Answer.class

---
+++ src/main/java/br/com/challenge_alura_one_t6/AluraForum/entities/Answer.java
@@ -0,0 +1,38 @@
+package br.com.challenge_alura_one_t6.AluraForum.entities;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+public class Answer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private  String message;
+    private LocalDateTime createdAt = LocalDateTime.now();
+    private boolean solved = false;
+
+    private Topic topic;
+
+    private User author;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/br/com/challenge_alura_one_t6/AluraForum/entities/TopicStatus.java
@@ -0,0 +1,9 @@
+package br.com.challenge_alura_one_t6.AluraForum.entities;
+
+public enum TopicStatus {
+
+    NOT_ANSWERED,
+    NOT_SOLVED,
+    SOLVED,
+    CLOSED;
+}
